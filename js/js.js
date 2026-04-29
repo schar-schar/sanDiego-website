e92079262de907af0c09cd2cd040ef13197de47c
schar-schar <b81harliec@aol.com>
2026-04-29 13:14:20 -0700

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,43 @@
+//background random color-colorchanger
+const htmlBody = document.querySelector("body");
+
+const randomClickFunction = function() {
+	const colors = ["#EC7272","#850E35","#850E35","#C1D5A4","#8FBDD3","#9FC088","#CC9544","#9D5353"];
+	
+	const randomIndex = Math.floor(Math.random() * colors.length);
+	
+	const randomColor = colors[randomIndex];
+	
+	htmlBody.style.background = randomColor;
+	
+	
+}
+htmlBody.onclick = randomClickFunction;//background random color-colorchanger
+const htmlBody = document.querySelector("body");
+
+const randomClickFunction = function() {
+	const colors = ["#EC7272","#850E35","#850E35","#C1D5A4","#8FBDD3","#9FC088","#CC9544","#9D5353"];
+	
+	const randomIndex = Math.floor(Math.random() * colors.length);
+	
+	const randomColor = colors[randomIndex];
+	
+	htmlBody.style.background = randomColor;
+	
+	
+}
+htmlBody.onclick = randomClickFunction;//background random color-colorchanger
+const htmlBody = document.querySelector("body");
+
+const randomClickFunction = function() {
+	const colors = ["#EC7272","#850E35","#850E35","#C1D5A4","#8FBDD3","#9FC088","#CC9544","#9D5353"];
+	
+	const randomIndex = Math.floor(Math.random() * colors.length);
+	
+	const randomColor = colors[randomIndex];
+	
+	htmlBody.style.background = randomColor;
+	
+	
+}
+htmlBody.onclick = randomClickFunction;
